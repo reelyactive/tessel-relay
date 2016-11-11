5c1c2a34e845654138b64b723c10df2adcb212e5
jeffyactive <jeff@reelyactive.com>
2016-11-10 20:59:33 -0500

    Using eventqueue to optimise bandwidth

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ var reelib = require('reelib');
 var options = { hostname: 'pareto.reelyactive.com', port: 80 };
 
 // See barnowl documentation for additional options
-var middleware = new barnowl( { enableMixing: true } );
+var middleware = new barnowl( { enableMixing: true,
+                                mixingDelayMilliseconds: 1000 } );
+
+// See reelib documentation for options
+var eventqueue = new reelib.event.eventqueue({
+    keepAliveMilliseconds: 2500,
+    disappearanceMilliseconds: 15000
+});
  
 // Expect a USB hub to be connected
 middleware.bind( { protocol: 'serial', path: '/dev/ttyUSB0' } ); 
@@ -20,10 +27,14 @@ middleware.bind( { protocol: 'serial', path: '/dev/ttyUSB0' } );
 // Green LED is on when detection events are relayed to remote server
 middleware.on('visibilityEvent', function(tiraid) {
   if(reelib.tiraid.isValid(tiraid)) {
-    tessel.led[2].on();
-    var event = { event: 'appearance', tiraid: tiraid };
-    reelib.event.postUpdate(options, event, function(err, event, message) {
-      tessel.led[2].off();
+    eventqueue.insert(tiraid, function(event) {
+      if(event) {
+        tessel.led[2].on();
+        event.tiraid = tiraid;
+        reelib.event.postUpdate(options, event, function(err, event, message) {
+          tessel.led[2].off();
+        });
+      }
     });
   }
 });
